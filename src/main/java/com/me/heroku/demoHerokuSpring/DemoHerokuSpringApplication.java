98a4e506b17960fab165c9d1a9d09ac85e6114b7
Hoi <Hoitnhe130173@fpt.edu.vn>
2020-12-28 22:36:50 +0700

    ok

---
+++ src/main/java/com/me/heroku/demoHerokuSpring/DemoHerokuSpringApplication.java
@@ -0,0 +1,13 @@
+package com.me.heroku.demoHerokuSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuSpringApplication.class, args);
+	}
+
+}

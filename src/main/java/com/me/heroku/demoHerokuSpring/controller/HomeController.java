4b3f2bfc09678322a69e6e9b05b2bae4c0c47084
VuaVoDanh729 <48729858+VuaVoDanh729@users.noreply.github.com>
2020-12-28 22:50:31 +0700

    Update HomeController.java

--- src/main/java/com/me/heroku/demoHerokuSpring/controller/HomeController.java
+++ src/main/java/com/me/heroku/demoHerokuSpring/controller/HomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/home")
+
 public class HomeController {
 
-    @GetMapping(value = "")
+    @GetMapping(value = "/")
     public String home(){
         return "home";
     }
